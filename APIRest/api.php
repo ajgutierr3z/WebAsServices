f2ca0fa49bb8c408de415db8fd3163f166ee207d
Celia Méndez Álvarez <celia@Ordenador-portatil-de-Celia.local>
2026-05-25 22:26:56 -0600

    mi practica - [testing of frexus]

--- APIRest/api.php
+++ APIRest/api.php
@@ -6,7 +6,7 @@ header("Content-Type: application/json");
 
 // 2. Permitir que cualquier aplicación acceda a esta API (CORS)
 header("Access-Control-Allow-Origin: *");
-
+//aquí debería de ir las modificaciones que se vayan realizando...
 // 3. Crear el mensaje de "Hola Mundo"
 $respuesta = [
     "status" => "success",
