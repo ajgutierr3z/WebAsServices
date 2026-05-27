bfc6e7b51d23ff0786de5834f657560550603d0e
AlexisLopezPerez <76860760+AlexisLopezPerez@users.noreply.github.com>
2026-05-27 13:48:13 -0600

    Mi proyecto [Alexis Lopez Perez]

--- APIs/pokemon/app.js
+++ APIs/pokemon/app.js
@@ -5,23 +5,25 @@ async function cargarDatos() {
     contenedor.innerHTML = 'Cargando...';
 
     try {
-        const respuesta = await fetch('https://rickandmortyapi.com/api/character');
+        const respuesta = await fetch('https://pokeapi.co/api/v2/pokemon/');
         const datos = await respuesta.json();
         
         contenedor.innerHTML = ''; // Limpiar el "Cargando..."
 
         // Iterar y filtrar solo los que están "Alive"
-        datos.results.forEach(personaje => {
-            if (personaje.status === 'Alive') {
+        datos.results.forEach(personaje => {                
                 const tarjeta = document.createElement('div');
                 tarjeta.className = 'tarjeta';
-                tarjeta.innerHTML = `
+                /*tarjeta.innerHTML = `
                     <img src="${personaje.image}" alt="${personaje.name}">
                     <h3>${personaje.name}</h3>
                     <p>Especie: ${personaje.species}</p>
+                `;*/
+                tarjeta.innerHTML = `                                      
+                    <h3>${personaje.name}</h3>
                 `;
                 contenedor.appendChild(tarjeta);
-            }
+            
         });
     } catch (error) {
         console.error("Error en la petición:", error);
