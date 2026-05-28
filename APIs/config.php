95b07eb1f2ca2e495dc71495c8f8024aec1b5dc0
AlexisLopezPerez <76860760+AlexisLopezPerez@users.noreply.github.com>
2026-05-27 19:27:22 -0600

    Revert "Ejercicio NASA"
    This reverts commit a2a36704010ae9064cf482f002d50a7cee8f6f97.

---
+++ APIs/config.php
@@ -0,0 +1,4 @@
+<?php
+define('OPENWEATHER_API_KEY', 'pega_aqui_tu_personal_access_token');
+define('GITHUB_TOKEN', 'pega_aqui_tu_personal_access_token');
+?>
--- APIs/nasa/config.php
+++ APIs/nasa/config.php
@@ -1,4 +1,4 @@
 <?php
 // Este archivo NO se sube a GitHub
-define('NASA_API_KEY', '');
+define('NASA_API_KEY', 'pega_aqui_tu_personal_access_token');
 ?>
