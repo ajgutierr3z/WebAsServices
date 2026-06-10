97d87d1466fb2ddda2ed3ca2f23d8aa20e389507
ajgutierrez <84606504+ajgutierr3z@users.noreply.github.com>
2026-06-10 15:51:37 -0600

    Delete APIs directory

--- APIs/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('OPENWEATHER_API_KEY', '.');
-define('GITHUB_TOKEN', 'pega_aqui_tu_personal_access_token');
-?>
--- APIs/nasa/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Este archivo NO se sube a GitHub
-define('NASA_API_KEY', 'pega_aqui_tu_personal_access_token');
-?>
